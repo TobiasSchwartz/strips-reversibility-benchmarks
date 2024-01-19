ca1b67facb5ba5363a8b000c6b7ca99e455f1f9d
Tobias Schwartz <tobias.schwartz@uni-bamberg.de>
2024-01-19 20:13:09 +0100

    improved subprocess handling in benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -2,66 +2,29 @@
 import subprocess
 import re
 
-def eval_domain_theirs(filename, horizon, timeoutLimit):
-    
-    c0 = f"mkdir -p artif_examples/"
-    subprocess.run([c0], text=True, capture_output=True, shell=True)
-    c1 = f"./plasp translate {filename} > ./artif_examples/{filename}.lp"
-    subprocess.run(c1, text=True, capture_output=True, shell=True)
-    c2 = f"/usr/bin/time -v ./clingo sequential-horizon.simple.elp -c horizon={horizon} artif_examples/{filename}.lp"
-    try:
-        output = subprocess.run(c2, text=True, capture_output=True, shell=True, timeout=timeoutLimit)
-        print(output)
-
-        wallClock = re.findall(r"Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): (.*?)\\n", str(output))[0]
-        print(wallClock)
-        setSize = re.findall(r"Maximum resident set size \(kbytes\): (.*?)\\n", str(output))[0]
-        print(setSize)
-
-        print(f"filename: {filename}")
-        print(f"wallClock: {wallClock}")
-        print(f"setSize: {setSize}")
-        return (filename, wallClock, setSize)
-
-    except TimeoutError:
-        return (filename, -1, -1)
-
-def eval_domain_ours(filename, strategy, horizon, timeoutLimit):
-    try:
-        c0 = f"/usr/bin/time -v ../reversible.py {filename} {strategy} {horizon}"
-        output = subprocess.run(c0, text=True, capture_output=True, shell=True, timeout=timeoutLimit)
-        print(output)
-        wallClock = re.findall(r"Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): (.*?)\\n", str(output))[0]
-        print(wallClock)
-        setSize = re.findall(r"Maximum resident set size \(kbytes\): (.*?)\\n", str(output))[0]
-        print(setSize)
-
-        print(f"filename: {filename}")
-        print(f"wallClock: {wallClock}")
-        print(f"setSize: {setSize}")
-        return (filename, wallClock, setSize)
-    except TimeoutError:
-        return (filename, -1, -1)
-
-
 def parseWallClock(output):
-    return re.findall(r"Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): (.*?)\\n", str(output))[0]
+    return re.findall(r"\s*Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): (.*?)\s*\n", output)[0]
 
 
 def parseSetSize(output):
-    return re.findall(r"Maximum resident set size \(kbytes\): (.*?)\\n", str(output))[0]
+    return re.findall(r"\s*Maximum resident set size \(kbytes\): (.*?)\s*\n", output)[0]
 
 
 def benchmark(approach, domainPath, reversibleActionName, horizon, timeoutLimit):
     if approach == "bfs" or approach == "dfs":
         try:
             command = f"/usr/bin/time -v python3 ./reversible.py {domainPath} {reversibleActionName} {approach} {horizon} True"
-            output = subprocess.run(command, text=True, capture_output=True, shell=True, timeout=timeoutLimit)
-            print(output.stdout)
-            wallClock = parseWallClock(output)
-            setSize = parseSetSize(output)
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = process.communicate(timeout=timeoutLimit)
+            stdout = stdout.decode('utf-8')
+            stderr = stderr.decode('utf-8')
+            print(stdout)
+            wallClock = parseWallClock(stderr)
+            setSize = parseSetSize(stderr)
+            print(f"Time: {wallClock} sec., Memory: {int(setSize)/1014:.2f} MB.")
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, wallClock, setSize)
-        except (TimeoutError, subprocess.TimeoutExpired):
+        except subprocess.TimeoutExpired:
+            process.kill()
             print(f"TimeoutError after {timeoutLimit} seconds")
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, -1, -1)
         
@@ -71,13 +34,16 @@ def benchmark(approach, domainPath, reversibleActionName, horizon, timeoutLimit)
 
         c2 = f"/usr/bin/time -v /tools/clingo /tools/sequential-horizon.simple.asp -c horizon={horizon} {domainPath}.lp"
         try:
-            output = subprocess.run(c2, text=True, capture_output=True, shell=True, timeout=timeoutLimit)
-            print(output.stdout)
-            # print(" - ".join(output.stdout.split("\n")[4:6]))
-            wallClock = parseWallClock(output)
-            setSize = parseSetSize(output)
+            process = subprocess.Popen(c2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = process.communicate(timeout=timeoutLimit)
+            stdout = stdout.decode('utf-8')
+            stderr = stderr.decode('utf-8')
+            print(stdout)
+            wallClock = parseWallClock(stderr)
+            setSize = parseSetSize(stderr)
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, wallClock, setSize)
         except (TimeoutError, subprocess.TimeoutExpired):
+            process.kill()
             print(f"TimeoutError after {timeoutLimit} seconds")
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, -1, -1)
         
@@ -88,12 +54,16 @@ def benchmark(approach, domainPath, reversibleActionName, horizon, timeoutLimit)
         # use --opt-mode=optN to get all optimal plans
         c2 = f"/usr/bin/time -v /tools/clingo /tools/sequential-horizon.general.alt.asp -c horizon={horizon} {domainPath}.lp"
         try:
-            output = subprocess.run(c2, text=True, capture_output=True, shell=True, timeout=timeoutLimit)
-            print(" - ".join(output.stdout.split("\n")[4:6]))
-            wallClock = parseWallClock(output)
-            setSize = parseSetSize(output)
+            process = subprocess.Popen(c2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = process.communicate(timeout=timeoutLimit)
+            stdout = stdout.decode('utf-8')
+            stderr = stderr.decode('utf-8')
+            print(stdout)
+            wallClock = parseWallClock(stderr)
+            setSize = parseSetSize(stderr)
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, wallClock, setSize)
-        except (TimeoutError, subprocess.TimeoutExpired):
+        except (subprocess.TimeoutExpired):
+            process.kill()
             print(f"TimeoutError after {timeoutLimit} seconds")
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, -1, -1)
         
@@ -103,15 +73,19 @@ def benchmark(approach, domainPath, reversibleActionName, horizon, timeoutLimit)
 
         c2 = f"/usr/bin/time -v java -jar /tools/qasp-0.1.2.jar -mn 1 {domainPath}.qasp"
         try:
-            output = subprocess.run(c2, text=True, capture_output=True, shell=True, timeout=timeoutLimit)
+            process = subprocess.Popen(c2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, stderr = process.communicate(timeout=timeoutLimit)
+            stdout = stdout.decode('utf-8')
+            stderr = stderr.decode('utf-8')
             # retrieve plan from output
-            plan = re.findall(r'occurs\("(.*?)",', str(output.stdout))
-            print(output.stdout.split("\n")[0])
+            plan = re.findall(r'occurs\("(.*?)",', stdout)
+            print(stdout.split("\n")[0])
             print(f"Plan: {'->'.join(plan[1:])}")
-            wallClock = parseWallClock(output)
-            setSize = parseSetSize(output)
+            wallClock = parseWallClock(stderr)
+            setSize = parseSetSize(stderr)
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, wallClock, setSize)
-        except (TimeoutError, subprocess.TimeoutExpired):
+        except subprocess.TimeoutExpired:
+            process.kill()
             print(f"TimeoutError after {timeoutLimit} seconds")
             return (domainPath, approach, reversibleActionName, horizon, timeoutLimit, -1, -1)
 
